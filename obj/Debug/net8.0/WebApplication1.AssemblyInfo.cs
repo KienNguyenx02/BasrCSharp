b1df43444d48d92372436fdedd8094fe3272c7ae
thanh1709 <thanhvtp2004@gmail.com>
2025-08-05 01:20:33 +0700

    init source

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b6b952dd14c633ab41da3e1018517b02c1c475")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50509d1caa41554c0e081e9e1d6e545a373c5dee")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
